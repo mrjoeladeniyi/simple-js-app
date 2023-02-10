f314e4cef8a61310898a13199aedf4073a95fdbe
mrjoeladeniyi <97857345+mrjoeladeniyi@users.noreply.github.com>
2023-02-10 23:37:06 +0100

    chore: Create pokemon objects
    Created array with a list of pokemons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Charizard', type: ['fire', 'flying' ], height: 1.7},
+    {name: 'Nidoking', type: ['ground', 'poison'], height: 1.4},
+    {name: 'Golem', type:['ground', 'rock'], height: 1.4},
+    {name: 'Fearow', type:['flying', 'normal'], height: 1.2},
+];
