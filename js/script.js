3a49062e154a43295d12b37c224c964d371626d2
mrjoeladeniyi <97857345+mrjoeladeniyi@users.noreply.github.com>
2023-02-14 14:11:49 +0100

    feat:  Add features to the pokémon app
    Added styling to the page

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ let pokemonList = [
     {name: 'Golem', type:['ground', 'rock'], height: 1.4},
     {name: 'Fearow', type:['flying', 'normal'], height: 1.2},
 ];
+
+// create for loop to iterate through pokemonList array and display it
+for (let i=0; i < pokemonList.length; i++){
+    let pokemonHeight = " ("+"height: " + pokemonList[i].height +")" + "<br>";
+
+    // conditional statement to highlight biggest pokémon
+    if (pokemonList[i].height > 1.5){
+        document.write("<h2>" + pokemonList[i].name + "</h2>" + pokemonHeight + "Wow, that's big!")
+    }else{
+        document.write("<h2>" + pokemonList[i].name + "</h2>" + pokemonHeight)
+    }
+}
+
+// Highlight favorite pokémon
