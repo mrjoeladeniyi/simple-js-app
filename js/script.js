3b794c7000af0f0ae0018463fffc6185f3525253
mrjoeladeniyi <97857345+mrjoeladeniyi@users.noreply.github.com>
2023-02-24 20:20:35 +0000

    refactor: Update variable to reflect new function
    Updated pokemonList for loop to reference pokemonRepository and new getAll method.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-let pokemonList = [
+
+// using IIFE to create a self-executing function()
+let pokemonRepository = (function() {
+
+    let pokemonList = [
     {name: 'Charizard', type: ['fire', 'flying' ], height: 1.7},
     {name: 'Nidoking', type: ['ground', 'poison'], height: 1.4},
     {name: 'Golem', type:['ground', 'rock'], height: 1.4},
     {name: 'Fearow', type:['flying', 'normal'], height: 1.2},
-];
+    ]
 
-// create for loop to iterate through pokemonList array and display it
-/*
-for (let i=0; i < pokemonList.length; i++){
-    let pokemonHeight = " ("+"height: " + pokemonList[i].height +")" + "<br>";
-
-    // conditional statement to highlight biggest pokémon
-    if (pokemonList[i].height > 1.5){
-        document.write("<h2>" + pokemonList[i].name + "</h2>" + pokemonHeight + "Wow, that's big!")
-    }else{
-        document.write("<h2>" + pokemonList[i].name + "</h2>" + pokemonHeight)
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-*/
 
-// refactor for loop using forEach
+    function getAll() {
+        return pokemonList;
+    }
+
+    return{
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+// create new variable with the function pokemonRepository
+let pokemonList = pokemonRepository.getAll();
 
 pokemonList.forEach(function(pokemon) {
     // document.write("<h2>" + pokemon.name + "</h2>" + "height:" + pokemon.height + "<br>");
@@ -29,4 +36,5 @@ pokemonList.forEach(function(pokemon) {
     }
 });
 
-// Highlight favorite pokémon
+// testing if the new function logs to console
+console.log(pokemonRepository.getAll());
