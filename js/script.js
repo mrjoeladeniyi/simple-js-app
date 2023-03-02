4b0733e137e461f8e5acbc2dbff01d3953d3c566
mrjoeladeniyi <97857345+mrjoeladeniyi@users.noreply.github.com>
2023-03-02 21:11:42 +0000

    refactor: Updated event listeners and functions

--- js/script.js
+++ js/script.js
@@ -11,30 +11,76 @@ let pokemonRepository = (function() {
 
     function add(pokemon) {
         pokemonList.push(pokemon);
+
+        // working on checking the condition of typeof object
+        // typeof pokemon === 'object';
+        /*
+        if (typeof pokemonInput === 'object') {
+            return (pokemon + ' is not an object');
+        }else{
+            pokemonInput;
+            console.log(typeof pokemon);
+        }
+        */
     }
 
     function getAll() {
         return pokemonList;
     }
 
+    function addListItem(pokemon) {
+        appEventListener(pokemon);
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon.name);
+    }
+
+    // added evenListner to condition
+    function appEventListener(pokemon){
+        let listOfPokemons = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.classList.add('pokemon-button');
+        button.addEventListener('click', function(){
+                showDetails(pokemon);
+            });
+        listOfPokemons.appendChild(listItem);
+        listItem.appendChild(button);
+
+        if (pokemon.height > 1.5){
+            button.innerText = pokemon.name + "\n" + "Wow! that's big";
+        }else{
+            button.innerText = pokemon.name;
+        }
+    }
+
     return{
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails,
+        appEventListener: appEventListener
     };
 
 })();
 
+pokemonRepository.add({name: 'Joel', type: ['fire'], height: 2.0})
+
 // create new variable with the function pokemonRepository
 let pokemonList = pokemonRepository.getAll();
 
+// loop function forEach item in pokemonList array
 pokemonList.forEach(function(pokemon) {
-    // document.write("<h2>" + pokemon.name + "</h2>" + "height:" + pokemon.height + "<br>");
-    if (pokemon.height > 1.5){
-        document.write("<h2>" + pokemon.name + "</h2>" + pokemon.height + "<br>" + "Wow, that's big!")
-    }else{
-        document.write("<h2>" + pokemon.name + "</h2>" + " height:" + pokemon.height + "<br>");
-    }
+    pokemonRepository.addListItem(pokemon)
 });
 
 // testing if the new function logs to console
 console.log(pokemonRepository.getAll());
+
+
+let checkType = typeof(value);
+
+
+
+console.log(pokemonList.length)
