ea267fdacc1003305e5c14205f7e1586abc93287
mrjoeladeniyi <97857345+mrjoeladeniyi@users.noreply.github.com>
2023-04-05 18:02:13 +0100

    chore: Update api to show 150 pokemons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // using IIFE to create a self-executing function()
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20";
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   function add(pokemon) {
     // working on checking the condition of typeof object
